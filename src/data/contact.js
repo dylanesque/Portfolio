e81d7a0288a94e445e6b9b0448a899e9ca2facdd
Michael Caveney <dylanesque27@gmail.com>
2021-01-14 13:13:32 -0500

    initial commit

---
+++ src/data/contact.js
@@ -0,0 +1,7 @@
+const data = {
+  github: 'https://github.com/dylanesque',
+  linkedin: 'https://www.linkedin.com/in/michael-caveney/',
+  email: 'michaelscaveney@gmail.com',
+}
+
+export default data
